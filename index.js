9a8a14e2df795145028055fda772fbe54d96752d
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-09-22 20:58:59 +0400

    phonebook backend step2

--- index.js
+++ index.js
@@ -36,6 +36,13 @@ app.get('/api/persons', (request, response) => {
     response.json(persons);
 });  
 
+app.get('/info', (request, response) => {
+    response.send(`<div>
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+    </div>`);
+});
+
 
 const PORT = 3002;
 app.listen(PORT, () => {
